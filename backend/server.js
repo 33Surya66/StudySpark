59f4809b75b6d8249a904133c4747a4edf9f8c8b
33Surya66 <surya03.ravi@gmail.com>
2025-08-07 20:35:01 +0530

    mong

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const analyticsRoutes = require('./routes/analyticsRoutes');
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
